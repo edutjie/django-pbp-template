0a340ecb78a58c261fa80575a9aa8fcf89038cbf
edutjie <edutjitrahardja@gmail.com>
2022-09-15 14:25:00 +0700

    added show xml by id

--- wishlist/views.py
+++ wishlist/views.py
@@ -31,3 +31,10 @@ def show_json_by_id(request, id):
     return HttpResponse(
         serializers.serialize("json", data), content_type="application/json"
     )
+
+
+def show_xml_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(
+        serializers.serialize("xml", data), content_type="application/xml"
+    )
