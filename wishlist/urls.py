674169edb67081f6ce8fcd45a512c4ecd9663ee7
edutjie <edutjitrahardja@gmail.com>
2022-09-21 22:34:08 +0700

    Tutorial 3 done!

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -5,6 +5,9 @@ from wishlist.views import (
     show_json,
     show_json_by_id,
     show_xml_by_id,
+    register,
+    login_user,
+    logout_user,
 )
 
 app_name = "wishlist"
@@ -15,4 +18,7 @@ urlpatterns = [
     path("json/", show_json, name="show_json"),
     path("json/<int:id>", show_json_by_id, name="show_json_by_id"),
     path("xml/<int:id>", show_xml_by_id, name="show_xml_by_id"),
+    path("register/", register, name="register"),
+    path("login/", login_user, name="login"),
+    path("logout/", logout_user, name="logout"),
 ]
